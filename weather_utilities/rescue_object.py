f2afda6ad973d17bd1394e0312f7fe987cceda87
Kimberly Mahoney <mahoneykimberlya@gmail.com>
2024-03-24 16:49:56 -0700

    adding the rescue object just in case

---
+++ weather_utilities/rescue_object.py
@@ -0,0 +1,27 @@
+from geopy.location import Location
+
+def geo_data():
+    data = [{'Taiwan': Location("臺灣", (23.5983227, 120.83537694479215, 0.0), raw = {})},
+    {'Madagascar': Location("Madagasikara / Madagascar", (-18.9249604, 46.4416422, 0.0), raw = {})},
+    {'Nicaragua': Location("Nicaragua", (12.6090157, -85.2936911, 0.0), raw = {})},
+    {'Myanmar': Location("မြန်မာ", (17.1750495, 95.9999652, 0.0), raw = {})},
+    {'Peru': Location("Perú", (-6.8699697, -75.0458515, 0.0), raw = {})},
+    {'Tanzania, United Republic Of': Location("Tanzania", (-6.5247123, 35.7878438, 0.0), raw = {})},
+    {'Laos': Location("ປະເທດລາວ", (20.0171109, 103.378253, 0.0), raw = {})},
+    {'Thailand': Location("Thailand, Banisilan, North Cotabato, Soccsksargen, Philippines", (7.4931858, 124.724704, 0.0), raw = {})},
+    {'Panama': Location("Panamá", (8.559559, -81.1308434, 0.0), raw = {})},
+    {'Mexico': Location("Ciudad de México, México", (19.4326296, -99.1331785, 0.0), raw = {})},
+    {'Indonesia': Location("Indonesia", (-2.4833826, 117.8902853, 0.0), raw = {})},
+    {'Guatemala': Location("Guatemala", (15.5855545, -90.345759, 0.0), raw = {})},
+    {'Honduras': Location("Honduras", (15.2572432, -86.0755145, 0.0), raw = {})},
+    {'Uganda': Location("Uganda", (1.5333554, 32.2166578, 0.0), raw = {})},
+    {'Ethiopia': Location("ኢትዮጵያ", (10.2116702, 38.6521203, 0.0), raw = {})},
+    {'United States (Hawaii)': Location("Hawaii, United States", (19.593801499999998, -155.42837009716908, 0.0), raw = {})},
+    {'Brazil': Location("Brasil", (-10.3333333, -53.2, 0.0), raw = {})},
+    {'Kenya': Location("Kenya", (1.4419683, 38.4313975, 0.0), raw = {})},
+    {'Costa Rica': Location("Costa Rica", (10.2735633, -84.0739102, 0.0), raw = {})},
+    {'El Salvador': Location("El Salvador", (13.8000382, -88.9140683, 0.0), raw = {})},
+    {'Colombia': Location("Colombia", (4.099917, -72.9088133, 0.0), raw = {})},
+    {'Vietnam': Location("Việt Nam", (15.9266657, 107.9650855, 0.0), raw = {})}]
+
+    return data
